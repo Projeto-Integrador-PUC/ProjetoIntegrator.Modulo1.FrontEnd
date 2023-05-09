a08b0c152b18cff4573e025eae8e6fb37629042c
Leonardo Couto Molinar Henrique <leo_couto29@hotmail.com>
2023-05-08 23:34:41 -0300

    Ajustando modulos e background-color do home.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AjudaComponent } from './rotas/ajuda/ajuda.component';
 import { CarrinhoComponent } from './rotas/carrinho/carrinho.component';
-
-import { HomeComponent } from './rotas/home/home.component';
 import { LojaComponent } from './rotas/loja/loja.component';
 import { SobreComponent } from './rotas/sobre/sobre.component';
 
 
 const rotas: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', loadChildren: () => import('./rotas/home/home.module').then(m => m.HomeModule) },
   { path: 'sobre', component: SobreComponent },
   { path: 'loja', component: LojaComponent },
   { path: 'ajuda', component: AjudaComponent },
