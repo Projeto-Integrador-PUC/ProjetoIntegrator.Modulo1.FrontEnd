efee9db445cfea4fb9d7208e322a8574c4da87fb
Leonardo Couto Molinar Henrique <leo_couto29@hotmail.com>
2023-05-11 15:57:33 -0300

    Ajustando eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    env: {
+        node: true,
+        es6: true,
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+        '@typescript-eslint/no-empty-function': 'warn',
+        '@typescript-eslint/no-unused-expressions': 'warn',
+        'no-console': 'warn',
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        'no-unused-private-class-members': 'warn',
+        'no-unreachable': ['error'],
+        'no-sparse-arrays': ['error'],
+        'no-setter-return': ['error'],
+        'no-self-compare': ['error'],
+        'no-self-assign': ['error'],
+        'no-inner-declarations': ['error'],
+        'camelcase': ['error', { 'properties': 'always' }],
+    }
+};
